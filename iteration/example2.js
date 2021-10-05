3d898fa4ab9fba5ddfee2e3926f2a9005448e218
yuna__ <yuna744@gmail.com>
2021-10-05 19:06:36 +0900

    es6

---
+++ iteration/example2.js
@@ -0,0 +1,5 @@
+const names = ['멍멍이', '야옹이', '다람이', '멍뭉이'];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ iteration/example3.js
@@ -0,0 +1,17 @@
+// while
+let i = 0; 
+let isFun = false;
+
+// while(i < 10){
+//     console.log(i);
+//     i++;
+// }
+
+
+while(!isFun){ // false일때는 계속 반복해라 isFun === false
+    console.log(i);
+    i++;
+    if(i === 30){
+        isFun = true;
+    }
+}
